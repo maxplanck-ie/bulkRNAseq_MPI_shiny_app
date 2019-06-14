2d33ef198b8fc5d1ddcba3d945556f478123cbe5
katsikora <sikora@pc329.ie-freiburg.mpg.de>
2019-06-14 09:47:24 +0200

    added sampleInfo checks

--- app.R
+++ app.R
@@ -61,6 +61,15 @@ server <- function(input, output, session) {
       return(res)
     }
     
+    check_sampleInfo<-function(sampleSheet){
+      if (all(sum(is.na(sampleSheet$Group))<nrow(sampleSheet),min(as.numeric(table(sampleSheet$Group)))>=2,length(table(sampleSheet$Group))>=2)){
+        res<-TRUE
+      }else{
+        res<-FALSE
+      }
+      return(res)
+    }
+    
    #######################################################################################  
     observeEvent(input$countfile, {
         
@@ -125,6 +134,11 @@ server <- function(input, output, session) {
         ######################
             observeEvent(input$runanalysis, {
                 sampleInfo<-isolate(values[["DF"]])
+                sinfo_ok<-check_sampleInfo(sampleInfo)
+                if(!isTruthy(sinfo_ok)){showModal(modalDialog(title = "SAMPLE INFORMATION INCOMPLETE",
+                                                              "Please provide group information for at least 2 replicates in at least 2 groups.",
+                                                              easyClose = TRUE))}
+                req(sinfo_ok)
                 sampleInfo<-sampleInfo[!sampleInfo$Group %in% "NA",]
                 rownames(sampleInfo)<-sampleInfo$PlottingID
                 values$sampleInfo<-sampleInfo
@@ -186,7 +200,6 @@ server <- function(input, output, session) {
                     plotdataL<-melt(plotdata,id.vars=c("GeneID"),value.name="Log2CPM",variable.name="SampleID")#,"GeneSymbol"
                     plotdataL$Log2CPM<-as.numeric(plotdataL$Log2CPM)
                     plotdataL$Group<-sampleInfo$Group[match(plotdataL$SampleID,sampleInfo$PlottingID)]
-                    #plotdata.SE<-summarySE(data=plotdataL,measurevar="Log2CPM",groupvars=c("Group","GeneID"))
                     #plotdata.SE$GeneSymbol<-plotdata$GeneSymbol[match(plotdata.SE$GeneID,plotdata$GeneID)]
                 
                     ggplot(data=plotdataL,aes(x=eval(as.name(x_choice)),y=Log2CPM,group=Group,colour=Group))+geom_jitter(size=2,alpha=0.6,width=0.2,height=0.000001)+scale_fill_manual(values=c("#FFFFFF","#CCCCCC"))+theme(text = element_text(size=16),axis.text = element_text(size=14),axis.text.x=element_text(angle=90,vjust=0),axis.title = element_text(size=14)) +xlab(x_choice)
