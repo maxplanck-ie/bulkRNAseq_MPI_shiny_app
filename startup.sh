b308554e7a79735c52d401cab2fb4091cde430c5
thomasmanke <manke@ie-freiburg.mpg.de>
2021-11-19 15:12:42 +0100

    deleted mounts.py and startup.sh

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Set up the mounting
-python /usr/local/bin/mounts.py
-
-#R --no-save --no-restore
-#chown -R galaxy:bioinfo /root/bulkRNAseq
-#su galaxy -s /bin/bash -c 'R -e ''shiny::runApp\(\"/root/bulkRNAseq\",port=2525,host=\"0.0.0.0\"\)''' 
-R -e 'shiny::runApp("/root/bulkRNAseq",port=2525,host="0.0.0.0")'
